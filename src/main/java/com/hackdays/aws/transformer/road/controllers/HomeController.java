ac6ea37e230362a990cd79742fc2e2c19753a978
Rohit Patil <4prohit@gmail.com>
2018-06-27 06:27:19 +0800

    Added Home page

---
+++ src/main/java/com/hackdays/aws/transformer/road/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.hackdays.aws.transformer.road.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
