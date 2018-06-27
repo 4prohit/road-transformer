4576f12f71081307efe9e3e2e015b9095acdea6e
Hui May <gohhuimay@gmail.com>
2018-06-27 18:00:48 +0800

    comment out link to db - just use our emails

--- src/main/java/com/hackdays/aws/transformer/road/services/NotificationService.java
+++ src/main/java/com/hackdays/aws/transformer/road/services/NotificationService.java
@@ -1,6 +1,8 @@
 package com.hackdays.aws.transformer.road.services;
 
+import java.util.Arrays;
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,18 +27,23 @@ public class NotificationService {
 		this.subscriptionRepository = subscriptionRepository;
 	}
 	
+	private static final List<String> EMAIL_LIST = Arrays.asList("gohhuimay@gmail.com", "amitkumarsahoo@gmail.com", "4rohitpatil@gmail.com", "franseffendi@gmail.com");
+	
 	public void checkAndSendNotifications(StreetDetail streetDetail, float confidence) {
 		if (confidence < alertConfidence) {
 			logger.info(String.format("Confidence level of %s is bad, preparing to send emails...", confidence));
 			int locationId = streetDetail.getLocationId();
-			List<Subscription> subscriptions = subscriptionRepository.findByLocationId(locationId);
-			if (subscriptions.size() > 0) {
-				for (Subscription subscription: subscriptions) {
-					EmailService.sendBadRoadEmail(subscription.getEmail(), streetDetail, confidence);
-				}
-			} else {
-				logger.info(String.format("Nobody is subscribing to location ID: %d", locationId));
+			for (String email: EMAIL_LIST) {
+				EmailService.sendBadRoadEmail(email, streetDetail, confidence);
 			}
+//			List<Subscription> subscriptions = subscriptionRepository.findByLocationId(locationId);
+//			if (subscriptions.size() > 0) {
+//				for (Subscription subscription: subscriptions) {
+//					EmailService.sendBadRoadEmail(subscription.getEmail(), streetDetail, confidence);
+//				}
+//			} else {
+//				logger.info(String.format("Nobody is subscribing to location ID: %d", locationId));
+//			}
 		} else {
 			logger.info(String.format("Confidence level of %s is good!", confidence));
 		}
